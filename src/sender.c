bfb4d0b98239f226c0ffb550ead4babab894f3bd
aymeric baurain <aymeric baurain>
2017-10-23 09:26:39 +0200

    first version

--- src/sender.c
+++ src/sender.c
@@ -27,6 +27,49 @@ struct pseudo_header
 /*
     Generic checksum calculation function
 */
+void returnMyIp(char *source_ip)
+{
+    const char* google_dns_server = "8.8.8.8";
+    int dns_port = 53;
+
+    struct sockaddr_in serv;
+
+    int sock = socket ( AF_INET, SOCK_DGRAM, 0);
+
+    //Socket could not be created
+    if(sock < 0)
+    {
+        perror("Socket error");
+    }
+
+    memset( &serv, 0, sizeof(serv) );
+    serv.sin_family = AF_INET;
+    serv.sin_addr.s_addr = inet_addr( google_dns_server );
+    serv.sin_port = htons( dns_port );
+
+    int err = connect( sock , (const struct sockaddr*) &serv , sizeof(serv) );
+
+    struct sockaddr_in name;
+    socklen_t namelen = sizeof(name);
+    err = getsockname(sock, (struct sockaddr*) &name, &namelen);
+
+    char buffer[100];
+    const char* p = inet_ntop(AF_INET, &name.sin_addr, buffer, 100);
+
+    if(p != NULL)
+    {
+        printf("Local ip is : %s \n" , buffer);
+        strcpy(source_ip,buffer);
+    }
+    else
+    {
+        //Some error
+        printf ("Error number : %d . Error message : %s \n" , errno , strerror(errno));
+    }
+
+    close(sock);
+}
+
 void readFileToSend(char *dt, char nameFile[]){
     FILE* fichier = NULL;
 
