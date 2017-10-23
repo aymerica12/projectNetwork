b92c16da9f9ec5d594988195b92d5dc1e6a06dbc
aymeric baurain <aymeric baurain>
2017-10-23 09:13:53 +0200

    first version

--- src/sender.c
+++ src/sender.c
@@ -9,7 +9,7 @@
 #include<netinet/udp.h>   //Provides declarations for udp header
 #include<netinet/ip.h>    //Provides declarations for ip header
 #include <arpa/inet.h>
-#include "selectserver.c"
+#include "src/selectserver.c"
 #define TAILLE_MAX 512
 
 /*
